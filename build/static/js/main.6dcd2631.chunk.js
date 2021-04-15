4de4f0b144d4f4ec94871df5bad15b88e3eb3a1b
Sungbin710 <dltjs8649@gmail.com>
2021-04-15 17:54:02 +0900

    first

---
+++ build/static/js/main.6dcd2631.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);var c=n(0),s=n.n(c),p=n(3),r=n.n(p),a=n(1);var i=function(){return Object(a.jsx)("div",{className:"App"})};r.a.render(Object(a.jsx)(s.a.StrictMode,{children:Object(a.jsx)(i,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.6dcd2631.chunk.js.map
