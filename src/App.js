6e0c5f2538e2f12c7994ca521674f57f3a12f5fd
Sungbin710 <dltjs8649@gmail.com>
2021-04-22 15:55:25 +0900

    issue1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
+import './default.scss';
 
 function App() {
   return (
     <div className="App">
+        <Header/>
       <h2>
         hello world!
       </h2>
